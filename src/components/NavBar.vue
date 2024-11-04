2c5c2132021832ccff098d7a552af2c90c350e4e
Guga Dam <lesmonades@gmail.com>
2024-11-04 00:39:01 -0300

    menu mobile and desktop

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,11 @@
 <template>
   <v-app-bar app color="var(--primary-color)" dark>
+    <!-- Botão do Menu (Mobile) -->
+    <v-app-bar-nav-icon
+      @click.stop="toggleMenu"
+      class="d-md-none"
+    ></v-app-bar-nav-icon>
+
     <v-toolbar-title>
       <router-link to="/" class="brand-logo">
         <img src="" alt="Logo" class="logo" />
@@ -19,91 +25,86 @@
       >
         {{ item.name }}
       </v-btn>
-    </v-row>
 
-    <!-- Avatar e Logout (Desktop) -->
-    <v-menu v-if="isUserLoggedIn && !isMobile" offset-y>
-      <template #activator="{ props }">
-        <v-btn icon v-bind="props">
+      <!-- Avatar e Logout (Desktop) -->
+      <template v-if="isUserLoggedIn">
+        <v-menu offset-y>
+          <template #activator="{ props }">
+            <v-btn icon v-bind="props">
+              <v-avatar size="36">
+                <img :src="userAvatar" alt="User Avatar" class="avatar-img" />
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="goToUserSettings" link>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Perfil</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="logoutUser" link>
+              <v-list-item-icon>
+                <v-icon color="var(--secondary-color)">mdi-logout</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </template>
+    </v-row>
+  </v-app-bar>
+
+  <!-- Menu Lateral (Mobile) -->
+  <v-navigation-drawer
+    v-model="menuCollapsed"
+    :location="mobile ? 'bottom' : 'right'"
+    temporary
+  >
+    <v-list>
+      <!-- Itens do Menu -->
+      <v-list-item
+        v-for="(item, index) in filteredMenuItems"
+        :key="index"
+        :to="item.path"
+        link
+        @click="toggleMenu"
+      >
+        <v-list-item-title>{{ item.name }}</v-list-item-title>
+      </v-list-item>
+
+      <!-- Avatar e Logout (Mobile) -->
+      <v-divider v-if="isUserLoggedIn"></v-divider>
+      <v-list-item
+        v-if="isUserLoggedIn"
+        @click="goToUserSettings"
+        link
+      >
+        <v-list-item-avatar>
           <v-avatar size="36">
             <img :src="userAvatar" alt="User Avatar" class="avatar-img" />
           </v-avatar>
-        </v-btn>
-      </template>
-      <v-list class="submenu-list">
-        <v-list-item @click="goToUserSettings" link title="Perfil">
-          <template #prepend>
-            <v-icon>mdi-account</v-icon>
-          </template>
-        </v-list-item>
-        <v-list-item @click="logoutUser" link title="Logout">
-          <template #prepend>
-            <v-icon color="var(--secondary-color)">mdi-logout</v-icon>
-          </template>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-
-    <!-- Botão do Menu (Mobile) -->
-    <v-btn icon @click="toggleMenu" class="d-md-none">
-      <v-icon color="var(--accent-color)">mdi-menu</v-icon>
-    </v-btn>
-
-    <!-- Menu Lateral (Mobile) -->
-    <v-navigation-drawer
-      v-model="menuCollapsed"
-      <!-- app
-      temporary
-      right
-      class="d-md-none" -->
-    >
-      <v-list>
-        <!-- Itens do Menu -->
-        <v-list-item
-          v-for="(item, index) in filteredMenuItems"
-          :key="index"
-          :to="item.path"
-          link
-          @click="toggleMenu"
-        >
-          <v-list-item-content>
-            <v-list-item-title>{{ item.name }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <!-- Avatar e Logout (Mobile) -->
-        <v-divider v-if="isUserLoggedIn"></v-divider>
-        <v-list-item
-          v-if="isUserLoggedIn"
-          @click="goToUserSettings"
-          link
-          :title="username"
-        >
-          <template #prepend>
-            <v-avatar size="36">
-              <img :src="userAvatar" alt="User Avatar" class="avatar-img" />
-            </v-avatar>
-          </template>
-        </v-list-item>
-        <v-list-item
-          v-if="isUserLoggedIn"
-          @click="logoutUser"
-          link
-          title="Logout"
-        >
-          <template #prepend>
-            <v-icon color="var(--secondary-color)">mdi-logout</v-icon>
-          </template>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-app-bar>
+        </v-list-item-avatar>
+        <v-list-item-title>{{ username }}</v-list-item-title>
+      </v-list-item>
+      <v-list-item
+        v-if="isUserLoggedIn"
+        @click="logoutUser"
+        link
+      >
+        <v-list-item-icon>
+          <v-icon color="var(--secondary-color)">mdi-logout</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
-
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/store/auth';
+import { useDisplay } from 'vuetify';
 
 const menuCollapsed = ref(false);
 
@@ -128,7 +129,9 @@ const isUserLoggedIn = computed(() => authStore.isAuthenticated);
 // Remove 'Register' e 'Login' se o usuário estiver logado
 const filteredMenuItems = computed(() => {
   if (isUserLoggedIn.value) {
-    return menuItems.filter(item => item.name !== 'Register' && item.name !== 'Login');
+    return menuItems.filter(
+      (item) => item.name !== 'Register' && item.name !== 'Login'
+    );
   }
   return menuItems;
 });
@@ -142,24 +145,10 @@ function goToUserSettings() {
   router.push('/user-settings');
 }
 
-// Detecta se é dispositivo móvel
-const isMobile = ref(false);
-
-function updateIsMobile() {
-  isMobile.value = window.innerWidth < 960;
-}
-
-onMounted(() => {
-  updateIsMobile();
-  window.addEventListener('resize', updateIsMobile);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('resize', updateIsMobile);
-});
+// Utiliza o composable useDisplay do Vuetify para detectar o dispositivo
+const { mobile } = useDisplay();
 </script>
 <style scoped>
-/* Estilos gerais */
 .logo {
   height: 50px;
   width: 50px;
@@ -179,7 +168,6 @@ onUnmounted(() => {
   display: block;
 }
 
-/* Estilos para o App Bar */
 .v-app-bar {
   background-color: var(--primary-color);
 }
@@ -188,7 +176,6 @@ onUnmounted(() => {
   height: 40px;
 }
 
-/* Estilos para os botões do menu (Desktop) */
 .v-btn {
   color: var(--text-color);
 }
@@ -197,13 +184,12 @@ onUnmounted(() => {
   background-color: rgba(255, 255, 255, 0.1);
 }
 
-/* Estilos para o Menu Lateral (Mobile) */
 .v-navigation-drawer {
   background-color: var(--primary-color);
 }
 
 .v-navigation-drawer .v-list-item {
-  color: black;
+  color: var(--text-color);
 }
 
 .v-navigation-drawer .v-list-item:hover {
@@ -211,16 +197,15 @@ onUnmounted(() => {
 }
 
 .v-navigation-drawer .v-icon {
-  color: black;
+  color: var(--text-color);
 }
 
-/* Estilos para o Submenu do Avatar */
 .submenu-list {
   background-color: var(--primary-color);
 }
 
 .submenu-list .v-list-item {
-  color: black);
+  color: var(--text-color);
 }
 
 .submenu-list .v-list-item:hover {
@@ -228,10 +213,9 @@ onUnmounted(() => {
 }
 
 .submenu-list .v-icon {
-  color: black;
+  color: var(--text-color);
 }
 
-/* Estilos para o botão do menu (Mobile) */
 .d-md-none .v-btn {
   color: var(--accent-color);
 }
