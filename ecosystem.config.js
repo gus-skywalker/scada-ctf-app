7cf0e36cd84b1342824b49fda0ed4ef3e56fdbd9
Guga Dam <lesmonades@gmail.com>
2024-11-08 17:40:53 -0300

    ecosystem fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "scada-ctf-app-spa",
-        script: "server.cjs",
+        script: "server.js",
         env: {
           NODE_ENV: "production",
           PORT: 4000
